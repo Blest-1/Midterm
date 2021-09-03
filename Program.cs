7808c46fd0435b61f72feac331c4c7ef5a6aee42
Blest-1 <81591931+Blest-1@users.noreply.github.com>
2021-09-03 09:27:57 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Timers;
+
+namespace MidtermStopWatch
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+
+        {
+            var startKey = new StopWatch();
+            startKey.Start();
+
+            var stopwatch = new StopWatch();
+            stopwatch.Stop();
+
+            var resetKey = new StopWatch();
+            resetKey.Reset();
+        }
+    }
+}
+
---
+++ StopWatch.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Timers;
+
+namespace MidtermStopWatch
+{
+    class StopWatch
+    {
+        public int seconds = 0;
+        public int minutes = 0;
+        public int hours = 0;
+        public bool startKey = true;
+        public bool resetKey = false;
+        public bool leaveKey = false;
+
+        public void Start()
+
+        {
+            Console.WriteLine("Press Enter to Start");
+
+            while (startKey)
+            {
+                if (Console.ReadKey(true).Key == ConsoleKey.Enter)
+                {
+                    Stop();
+                }
+                break;
+            }
+        }
+
+
+        public void Stop()
+
+        {
+            Console.WriteLine("Spacebar to stop");
+
+            while (!(Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Spacebar))
+            {
+                Console.Write("\r Time: {00}:{1}:{2}", hours, minutes, seconds);
+                if (seconds == 60)
+                {
+                    minutes++;
+                    seconds = 0;
+                }
+                System.Threading.Thread.Sleep(1000);
+                seconds++;
+            }
+
+            Reset(); Start();
+        }
+
+
+        // Can only reset 1 time before terminal closes.
+        public void Reset()
+
+        {
+            Console.WriteLine("Esc to reset");
+            Console.WriteLine("BackSpace to Exit");
+
+            while (resetKey)
+            {
+
+                if (Console.ReadKey(true).Key == ConsoleKey.Escape)
+                {
+                    //Console.WriteLine("\nPeace Out");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+
+                if (Console.ReadKey(true).Key == ConsoleKey.Enter)
+                {
+                    Stop();
+                }
+                break;
+            }
+        }
+    }
+}
